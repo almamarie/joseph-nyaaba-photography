b957827eb92821b32d046a422c014445fb540549
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2024-03-06 18:25:46 +0000

    Completed mailing list section

--- src/components/pages/home-page/HomePage.jsx
+++ src/components/pages/home-page/HomePage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "./hero/Hero";
 import Gallery from "./gallery/Gallery";
 import Testimonials from "./testimonials/Testimonials";
+import MailingList from "./mailing-list/MailingList";
 
 const HomePage = () => {
   return (
@@ -9,6 +10,7 @@ const HomePage = () => {
       <Hero />
       <Gallery />
       <Testimonials />
+      <MailingList />
     </>
   );
 };
--- src/components/pages/home-page/mailing-list/MailingList.jsx
+++ src/components/pages/home-page/mailing-list/MailingList.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import styles from "./MailingList.module.css";
 
 const MailingList = () => {
-  return <div>MailingList</div>;
+  return (
+    <section className={styles.wrapper}>
+      <h2>JOIN THE MAILING LIST</h2>
+      <p>
+        Lorem Ipsum has been the industry's standard dummy text ever since the
+        1500s, when an unknown printer took a galley of type a
+      </p>
+      <div className={styles["input-wrapper"]}>
+        <input type="text" placeholder="Enter Your Email Address" />
+        <button type="text">SUBSCRIBE</button>
+      </div>
+    </section>
+  );
 };
 
 export default MailingList;
