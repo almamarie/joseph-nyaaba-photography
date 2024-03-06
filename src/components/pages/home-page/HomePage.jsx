355bcbb8f4a28ebb67391f7c9e9f23c655f3e4e3
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2024-03-06 16:53:43 +0000

    Leaving testimonial to work on mailing list

--- src/components/pages/home-page/HomePage.jsx
+++ src/components/pages/home-page/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "./hero/Hero";
 import Gallery from "./gallery/Gallery";
+import Testimonials from "./testimonials/Testimonials";
 
 const HomePage = () => {
   return (
     <>
       <Hero />
       <Gallery />
+      <Testimonials />
     </>
   );
 };
